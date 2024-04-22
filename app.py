3c2d2626cc76cabefefa900a2aea956cd0e8ab40
qjjslomibao <112042148+qjjslomibao@users.noreply.github.com>
2024-04-23 00:09:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-# app.py
 import requests
 from flask import Flask, render_template, request, session
 from datetime import datetime
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Necessary for session management
+app.secret_key = 'your_secret_key'  
 
 @app.route('/', methods=['GET', 'POST'])
-def test_index():
-    api_key = "767d97f0f74e48408bce2b8588833d41"  # Replace with your actual API key
+def index():
+    api_key = "767d97f0f74e48408bce2b8588833d41"  
     if 'search_history' not in session:
         session['search_history'] = []
-
+    
     ip_data = {}
     if request.method == 'POST':
         ip_address = request.form['ip']
@@ -33,7 +32,7 @@ def get_ip_info(api_key, ip_address=None):
     if ip_address:
         url += f"&ip={ip_address}"
     try:
-        response = requests.get(url, timeout=10)
+        response = requests.get(url)
         if response.status_code == 200:
             return response.json()
         else:
